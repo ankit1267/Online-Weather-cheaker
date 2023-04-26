0d4507c8c87d7ed55eb1a78b6c92e6a3a1d4a9f2
Ankit Dongre <72199330+ankit1267@users.noreply.github.com>
2023-04-27 00:13:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,74 @@
-let key = "eb29bffd05be4c4a38e7c6f71b90134b";
-
-let cityinput = document.querySelector(".cityinput");
-let checkbutton = document.querySelector(".checkbutton");
-let infobox = document.querySelector(".infobox");
-let cityname = document.querySelector(".cityname");
-let countryname = document.querySelector(".country");
-let temp = document.querySelector(".temp");
-let tempmin = document.querySelector(".tempmin");
-let tempmax = document.querySelector(".tempmax");
-let desc = document.querySelector(".desc");
-let windspeed = document.querySelector(".windspeed");
-let humidity = document.querySelector(".humidity");
-let pressure = document.querySelector(".pressure");
-let bgimg = document.querySelector(".bgimg")
-let morebtn = document.querySelector(".more");
-let details = document.querySelector(".details");
-
-const userAction = (city) =>{
-
-  if(city===""){
-    city= "delhi";
-  }
-
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-  .then(response => response.json())
-  .then(jsondata => {
-      
-      cityname.innerHTML = jsondata.name;
-      countryname.innerHTML = jsondata.sys.country;
-      temp.innerHTML =  (jsondata.main.temp -273.15).toFixed(1);
-      tempmin.innerHTML = (jsondata.main.temp_min -273.15).toFixed(1);
-      tempmax.innerHTML = (jsondata.main.temp_max -273.15).toFixed(1);
-      desc.innerHTML = jsondata.weather[0].description;
-      windspeed.innerHTML = jsondata.wind.speed;
-      humidity.innerHTML = jsondata.main.humidity;
-      pressure.innerHTML = jsondata.main.pressure;
-      
-      if(jsondata.weather[0].description.includes("cloud")||jsondata.weather[0].description.includes("rain")){
-        document.querySelector(".fa-cloud").style.display="block";
-        document.querySelector(".fa-bolt").style.display="none";
-        document.querySelector(".fa-sun").style.display="none";
-
-      }
-      else if(jsondata.weather[0].description.includes("thunderstorm")){
-        document.querySelector(".fa-cloud").style.display="none";
-        document.querySelector(".fa-bolt").style.display="block";
-        document.querySelector(".fa-sun").style.display="none";
-
-      }
-      else if(jsondata.weather[0].description.includes("sun")){
-        document.querySelector(".fa-cloud").style.display="none";
-        document.querySelector(".fa-bolt").style.display="none";
-        document.querySelector(".fa-sun").style.display="block";
-
-      }
-      else{
-        document.querySelector(".fa-cloud").style.display="none";
-        document.querySelector(".fa-bolt").style.display="none";
-        document.querySelector(".fa-sun").style.display="none";
-      }
-      bgimg.src = `https://source.unsplash.com/1600x900/?${jsondata.weather[0].description}`;
-  });
-
-  infobox.style.display = "block";
-  cityinput.value="";
-}
 
 
-checkbutton.addEventListener('click',()=>{
-    userAction(cityinput.value);
+$(document).ready(function() {
+    // Date Time
+    var date = new Date()
+    var month = date.getMonth() + 1
+    var day = date.getDate() 
+    var year = date.getFullYear()
+
+    $("#date").text(`${day}/${month}/${year}`)
+    var time = date.getHours() + ":" + date.getMinutes()
+
+    // good morning
+    // if (date.getHours() < 12) {
+    //     $("body").css("background-color", "#83d8fa")
+    // }
+    // // good afternoon
+    // else if(date.getHours() > 12 && date.getHours() <= 19) {
+    //     $("body").css("background-color", "#ffa366")
+    //     $("body").css("color", "#fff")
+    // }
+    // // good night
+    // else if(date.getHours() > 19) {
+    //     $("body").css("background-color", "#330867")
+    // }
+
+    var apiURL = ""
+    $("#search").click(function() {
+        var cityName = $("#city_name_input").val()
+        apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5e3cfd1c5c084afc370f31cf6a731cff&units=metric`
+        $("#city_name").text(cityName.charAt(0).toUpperCase() + cityName.slice(1))
+        
+        // API CALL
+        fetch(apiURL)
+        .then(response => response.json())
+        .then((data) => { 
+            $("#temp_value").text(data.main.temp);
+            console.log(data);
+            if(data.weather.main == "cloudy") {
+                console.log(cloudy);
+            }
+            // weather image status
+            console.log(data.weather[0].main)
+            if(data.weather[0].main == "Haze") {
+                $("#icon").html("<i class='fas fa-smog'></i>")
+                $("#weather_status").text("Haze")
+                $("body").css("color", "#fff")
+                $("#weather_status").css("font-size", "calc(10px + 2vh)")
+                $('.container').css('background-image', 'url(./img/haze.jpg)')
+            }
+            else if(data.weather[0].main == "Clouds") {
+                $("#icon").html("<i class='fas fa-cloud'></i>")
+                $("#weather_status").text("Cloudy")
+                $("body").css("color", "#fff")
+                $("#weather_status").css("font-size", "calc(10px + 2vh");
+                $('.container').css('background-image', 'url("./img/cloudy.jpg")');
+            }
+            else if(data.weather[0].main == "Rain") {
+                $("#icon").html("<i class='fas fa-cloud-rain'></i>")
+                $("#weather_status").text("Rainy")
+                $("#weather_status").css("font-size", "calc(10px + 2vh");
+                $('.container').css('background-image', 'url("./img/rain.jpg")');
+            } 
+            else if(data.weather[0].main == "Sunny") {
+                $("#icon").html("<i class='fas fa-cloud-rain'></i>")
+                $("#weather_status").text("Rainy")
+                $("body").css("color", "#fff")
+                $("#weather_status").css("font-size", "calc(10px + 2vh")
+                $('.container').css('background-image', 'url("./img/sunny.jpg")')
+            } 
+        })
+    })
 })
 
-morebtn.addEventListener('click',()=>{
-  if(details.style.display==="none" || details.style.display===""){
-    details.style.display="block";
-  }else if(details.style.display==="block"){
-    details.style.display="none";
-  }
-})
